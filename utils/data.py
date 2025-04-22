8f4280abbe1af04c7c9ddeba862a9b734a77479d
heisenbrook <matteo.vannini93@gmail.com>
2025-04-22 18:40:32 +0200

    modified data.py

--- utils/data.py
+++ utils/data.py
@@ -1,4 +1,5 @@
 from tvDatafeed import TvDatafeed, Interval
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import numpy as np
 from keys import user, psw
@@ -17,13 +18,39 @@ def RSI(n_candles, data):
     return rsi
 
 def Stoch_RSI(n_candles, rsi):
-        
     min_rsi = rsi.rolling(n_candles).min()
     max_rsi = rsi.rolling(n_candles).max()
     
     stoch_rsi = ((rsi - min_rsi) / (max_rsi - min_rsi)) * 100
     
     return stoch_rsi
+
+def cyclic_enc(data, col, period):
+    data[f'{col}_sin'] = np.sin(2 * np.pi * data[col] / period)
+    data[f'{col}_cos'] = np.cos(2 * np.pi * data[col] / period)
+    return data
+
+def preprocess(data):
+    data.drop('symbol', axis=1, inplace=True)
+
+    data['RSI'] = RSI(14, data)
+    data['Stoch_RSI'] = Stoch_RSI(14, data['RSI'])
+    #data['buy/sell'] = np.where(data['RSI'] + data['Stoch_RSI'] > 50, 'Sell', 'Buy')
+
+    min_max_scaler = MinMaxScaler()
+    prices = data[['open','high','low','close']]
+    prices_min_max = min_max_scaler.fit_transform(prices)
+
+    data['day_of_the_week'] = data.index.day % 7
+    data = cyclic_enc(data, 'day_of_the_week', 7)
+
+    data[['open','high','low','close']] = prices_min_max
+
+    data.insert(0, 'day_of_the_week', data.pop('day_of_the_week'))
+    data.insert(1, 'day_of_the_week_sin', data.pop('day_of_the_week_sin'))
+    data.insert(2, 'day_of_the_week_cos', data.pop('day_of_the_week_cos'))
+
+    
 
 
 tv = TvDatafeed(user, psw)
@@ -32,15 +59,13 @@ btcusdt = tv.get_hist(symbol='BTCUSDT',
                       exchange='BINANCE', 
                       interval=Interval.in_daily, 
                       n_bars=10000,
-                      extended_session=False)
+                      extended_session=True)
 
-btcusdt.drop('symbol', axis=1, inplace=True)
-
-btcusdt['RSI'] = RSI(14, btcusdt)
-btcusdt['Stoch_RSI'] = Stoch_RSI(14, btcusdt['RSI'])
-btcusdt['buy/sell'] = np.where(btcusdt['RSI'] + btcusdt['Stoch_RSI'] > 50, 'Sell', 'Buy')
+preprocess(btcusdt)
 
 btcusdt.to_csv('data.csv')
+
+
 
 
     
