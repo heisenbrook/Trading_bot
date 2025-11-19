8890b6d37a678e4b48007ba49dfe405997f1aab9
heisenbrook <matteo.vannini93@gmail.com>
2025-11-19 17:26:54 +0100

    data.py cleanup

--- utils/data.py
+++ utils/data.py
@@ -30,9 +30,6 @@ class BTCDataset(Dataset):
 
         self.prices_col = ['high', 'low', 'open', 'close']
         self.momentum_col = ['RSI']
-        # self.bands_col = ['BBANDS_UPPER', 'BBANDS_MIDDLE', 'BBANDS_LOWER', 'SMA_50', 'PLAW', 'PLAW_BANDS_LOW', 'PLAW_BANDS_UP']
-        # self.patterns_col = ['dist_nearest_support', 'dist_nearest_resistance', 'strength_support', 'strength_resistance']
-        # self.statistical_col = ['VAR', 'LINREG', 'STDDEV', 'TSF']
         self.volume_col = ['volume', 'OBV']
         self.target_col = ['next_close']
         self.feat_cols = self.data.columns.to_list()
@@ -53,13 +50,6 @@ class BTCDataset(Dataset):
                              ('power', PowerTransformer(method='yeo-johnson', standardize=True))
                     ]), self.volume_col),
                     ('momentum', StandardScaler(), self.momentum_col),
-                    #('patterns', StandardScaler(), self.patterns_col),
-                    #('bands', MinMaxScaler(), self.bands_col),
-                    #('statistical', Pipeline([
-                    #         ('robust', RobustScaler()),
-                    #         ('power', PowerTransformer(method='yeo-johnson', standardize=True))
-                    #]), self.statistical_col),
-                    #('targets', StandardScaler(), self.target_col)
                ],
                 remainder='passthrough'
               )
@@ -89,14 +79,6 @@ class BTCDataset(Dataset):
         
         num_targets = len(self.target_col)
         y_flat = y.reshape(-1, num_targets)
-        
-        
-        # transf = self.preprocessor.named_transformers_['targets']
-
-        # if isinstance(transf, Pipeline):
-        #     for _, step in reversed(transf.steps):
-        #         if hasattr(step, 'inverse_transform'):
-        #             y_flat = step.inverse_transform(y_flat)
         
         if isinstance(last_time_index, (list, np.ndarray)):
             all_dates = []
@@ -129,25 +111,6 @@ def preprocess(horizon, data: pd.DataFrame, is_inference=False):
 
     # Momentum indicators
     data['RSI'] = talib.RSI(data['close'], timeperiod=14)
-    # data['MOM'] = talib.MOM(data['close'], timeperiod=10)
-    # data['MACD'], data['MACD_SIGNAL'], data['MACD_HIST'] = talib.MACD(data['close'], fastperiod=12, slowperiod=26, signalperiod=9)
-    # data['ADX'] = talib.ADX(data['high'], data['low'], data['close'], timeperiod=14)
-    # data['ROC'] = talib.ROC(data['close'], timeperiod=10)
-
-    # Overlap indicators
-    # data['BBANDS_UPPER'], data['BBANDS_MIDDLE'], data['BBANDS_LOWER'] = talib.BBANDS(data['close'], timeperiod=20, nbdevup=2, nbdevdn=2, matype=0)
-    # data['SMA_50'] = talib.SMA(data['close'], timeperiod=50)
-    # data['PLAW'] = fit_power_law(data['close'])
-    # data['PLAW_BANDS_LOW'], data['PLAW_BANDS_UP'] = powerlaw_bands(data['PLAW'], data['close'])
-
-    # Pattern recognition indicators
-    # data = add_res_support_features(data, 14)
-
-    # Statitical indicators
-    # data['TSF'] = talib.TSF(data['close'], timeperiod=14)
-    # data['VAR'] = talib.VAR(data['close'], timeperiod=14, nbdev=1)
-    # data['LINREG'] = talib.LINEARREG(data['close'], timeperiod=14)
-    # data['STDDEV'] = talib.STDDEV(data['close'], timeperiod=14, nbdev=1)
 
     # Volume indicators
     data['OBV'] = talib.OBV(data['close'], data['volume'])
@@ -179,87 +142,7 @@ def create_labels(horizon, data: pd.DataFrame):
 
     return label
 
-# =============================================
-# Feature engineering functions
-# =============================================
 
-def power_law(x, a, b):
-    """
-    Power law function for curve fitting.
-    Used to model relationships in financial time series data.
-    See https://en.wikipedia.org/wiki/Power_law for more details.
-    Inspired by Giovanni Santostasi's work:
-    """
-    return a * ((x + np.finfo(float).eps)**b)
-
-def fit_power_law(data):
-    """
-    Fit a power law to the given data series.
-    """
-    x = np.arange(len(data))
-    y = data.values
-    valid = y > 0
-    x_fit = x[valid]
-    y_fit = y[valid]
-
-    params, _ = curve_fit(power_law, x_fit, y_fit, p0=[1, 1], bounds=([0.001, -5], [np.inf, 5]))
-
-    fitted_y = power_law(x, *params)
-
-    return fitted_y
-
-def powerlaw_bands(pw, close, window = 1460):
-    """
-    Calculate dynamic bands around a power law trend based on rolling standard deviation of residuals.
-    """
-    residual = (close - pw) / pw
-    rolling_std = residual.rolling(window=window, min_periods=1).std()
-    lower_band = pw * (1 - 1.5 * rolling_std)
-    upper_band = pw * (1 + 1.5 * rolling_std) 
-    return lower_band, upper_band
-
-
-def nearest_distance(price, levels):
-    """
-    Calculate the distance to the nearest support or resistance level.  
-    """
-    if len(levels) == 0:
-        return np.nan
-    return np.min(np.abs(price - levels))
-
-def strength_distance(price, levels, tolerance=0.01):
-    """
-    Calculate the strength of support or resistance based on proximity to multiple levels.    
-    """
-    if len(levels) == 0:
-        return np.nan
-    distances = np.abs(price - levels)
-    strengths = 1 / (distances + tolerance)
-    return np.sum(strengths)
-
-def add_res_support_features(data, window):
-    """
-    Identify local support and resistance levels and compute features based on their proximity.
-    """
-
-    data = data.copy()
-
-    data['local_min'] = data['low'].rolling(window, center=True).min()
-    data['local_max'] = data['high'].rolling(window, center=True).max()
-
-    data['is_support'] = (data['low'] == data['local_min'])
-    data['is_resistance'] = (data['high'] == data['local_max'])
-
-    supports = data[data['is_support']]['low'].values
-    resistances = data[data['is_resistance']]['high'].values
-
-    data['dist_nearest_support'] = data.apply(lambda row: nearest_distance(row['close'], supports), axis=1)
-    data['dist_nearest_resistance'] = data.apply(lambda row: nearest_distance(row['close'], resistances), axis=1)
-
-    data['strength_support'] = data.apply(lambda row: strength_distance(row['close'], supports), axis=1)
-    data['strength_resistance'] = data.apply(lambda row: strength_distance(row['close'], resistances), axis=1)
-
-    return data.drop(columns=['local_min', 'local_max', 'is_support', 'is_resistance'])
 
     
 
